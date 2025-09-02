b54175da5ed29a9fa41924c258f6b7ff1a0abfb3
TanviHanda <tanvisona2012@gmail.com>
2025-09-03 03:49:17 +0530

    setting up acme logo

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,12 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <p className={`${inter.className} text-2xl font-bold`}>hello everyone</p>
+      {/* <p className={`${inter.className} text-2xl font-bold`}>hello everyone</p>
       <p className={`${roboto.className} text-lg`}>How are you all?</p>
       <p className={`${lusitana.className} text-2xl font-bold`}>Welcome to the Acme Dashboard</p>
-      <div className={styles.shape2} />
+      <div className={styles.shape2} /> */}
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
