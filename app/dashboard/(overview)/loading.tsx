13d9a548da19df278c45645e8fb41757dfed9bbb
TanviHanda <tanvisona2012@gmail.com>
2025-09-04 11:20:52 +0530

    set loading on dashboard page [importing DashboardSkeleton in loading.tsx]

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,9 @@
+// export default function Loading(){
+//     return <div>Loading...</div>;
+// }
+
+// loading skeleton 
+import DashboardSkeleton from "../../ui/skeletons";
+export default function Loading(){
+    return <DashboardSkeleton/>;
+}
