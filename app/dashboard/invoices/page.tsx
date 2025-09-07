c26099f89d1f6aacbedda06bec983a2513297c2a
TanviHanda <tanvisona2012@gmail.com>
2025-09-07 16:35:05 +0530

    set meta data

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,8 +1,14 @@
+import { Metadata } from "next";
 import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
 import Table from "@/app/ui/invoices/table";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+
 import { Suspense } from "react";
 import { lusitana } from "@/app/ui/fonts";
 import { fetchInvoicesPages } from "@/app/lib/data";
